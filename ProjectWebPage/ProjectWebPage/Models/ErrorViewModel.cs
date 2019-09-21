ca4cea2efcf985bbf2bad7595e8594c80a478f15
ghostinit69 <53224646+ghostinit69@users.noreply.github.com>
2019-09-21 17:07:53 -0400

    Add files via upload
    The beginning...

---
+++ ProjectWebPage/ProjectWebPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectWebPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
